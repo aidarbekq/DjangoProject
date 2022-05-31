061acd5d71fde0836f2037e683b57a78a9796c45
user-aidarbek <j.aidarbek19@gmail.com>
2022-05-31 20:06:02 +0600

    Initial commit

---
+++ post/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='post_img')
+
+
+    def __str__(self):
+        return self.title
+
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+# Create your models here.
